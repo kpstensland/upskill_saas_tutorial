0b431ec38415548b629d745fc59c41b388f5ffdd
Kenneth Stensland <kenneth.p.stensland@gmail.com>
2016-12-24 17:14:32 +0000

    added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ActiveRecord::Base
+  #name, email, comments, created_at, updated_at (pulled from db automatically)
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
